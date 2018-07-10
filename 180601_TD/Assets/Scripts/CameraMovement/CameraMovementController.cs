8ace622159c4dc695d4057e05432dfecee2f3361
PavelSergaziev <dargoth@narod.ru>
2018-07-11 02:38:01 +0300

    Модульность контроллера камеры для поддержки разных платформ
    Добавил интерфейс модуля ввода для перемещения и зума камеры. Пока думаю, что будет один контроллер со всеми настройками для всех типов ввода (тач / мышь-клава и т.п.), а на старте игры в зависимости от платформы будет подставляться нужный модуль, метод из которого будет вызываться в апдейте контроллера.

--- 180601_TD/Assets/Scripts/CameraMovement/CameraMovementController.cs
+++ 180601_TD/Assets/Scripts/CameraMovement/CameraMovementController.cs
@@ -29,79 +29,43 @@ namespace Game
         [SerializeField]
         private float _zoomMaxCamHeight;
 
-        private CameraMovementModule _cameraMovement;
-
-        private float _rightBorder;
-        private float _leftBorder;
-        private float _topBorder;
-        private float _bottomBorder;
-
-        private Vector3 _mousePosition;
-        private float _horizontalAxisValue;
-        private float _verticalAxisValue;
-        private float _mouseScrollAxisValue;
-
-        //private const float ScreenSizeChangeCheckDelay = 1f;
+        private CameraMovementModule _cameraMovementModule;
+        private ICameraMovementInput _cameraInputModule;
+                
+        private Vector3 _cameraMovementVector;
+        private float _cameraZoomValue;
+        
 
         // Use this for initialization
         void Awake()
         {   
-            _cameraMovement = new CameraMovementModule(this.transform, _zoomMinCamHeight, _zoomMaxCamHeight);
-            SetMouseCamMoveBorderZones();
-            //InvokeRepeating(CheckForScreenSizeChange, ScreenSizeChangeCheckDelay, ScreenSizeChangeCheckDelay);
+            _cameraMovementModule = new CameraMovementModule(this.transform, _zoomMinCamHeight, _zoomMaxCamHeight);
+
+            //выбираем модуль ввода в зависимости от платформы
+#if UNITY_ANDROID
+            _cameraInputModule = null;
+#else
+            _cameraInputModule = new CameraMovementInputWindowsPC(_mouseMoveSideTriggerWidth);
+#endif
+                       
         }
-
-
-
-        // Update is called once per frame
-
+        
+        
         //не забыть выключить, когда мы в главном меню, а не в основной игре
         //проще всего это сделать, задизейблив скрипт при выходе в меню
         void Update()
         {
 
-            _mousePosition = Input.mousePosition;
+            _cameraInputModule.GetInput(ref _cameraMovementVector, ref _cameraZoomValue);
 
-            _horizontalAxisValue = Input.GetAxisRaw("Horizontal");
-            _verticalAxisValue = Input.GetAxisRaw("Vertical");
-            _mouseScrollAxisValue = Input.GetAxis("Mouse ScrollWheel");
+            if (_cameraMovementVector != Vector3.zero)
+                _cameraMovementModule.MoveCamera(_cameraMovementVector * _cameraMoveSpeed);
 
-            if (_mousePosition.x <= _leftBorder || _horizontalAxisValue < 0)
-                _cameraMovement.MoveCamera(new Vector3(-_cameraMoveSpeed, 0, 0));
-
-            if (_mousePosition.x >= _rightBorder || _horizontalAxisValue > 0)
-                _cameraMovement.MoveCamera(new Vector3(_cameraMoveSpeed, 0, 0));
-
-            if (_mousePosition.y <= _bottomBorder || _verticalAxisValue < 0)
-                _cameraMovement.MoveCamera(new Vector3(0, 0, -_cameraMoveSpeed));
-
-            if (_mousePosition.y >= _topBorder || _verticalAxisValue > 0)
-                _cameraMovement.MoveCamera(new Vector3(0, 0, _cameraMoveSpeed));
-
-            if (_mouseScrollAxisValue != 0)
-                _cameraMovement.ZoomCamera(_zoomSpeed * _mouseScrollAxisValue);
+            if (_cameraZoomValue != 0)
+                _cameraMovementModule.ZoomCamera(_cameraZoomValue * _zoomSpeed);
 
         }
-
-        private void SetMouseCamMoveBorderZones()
-        {
-            _leftBorder = Screen.width * _mouseMoveSideTriggerWidth;
-            _bottomBorder = Screen.height * _mouseMoveSideTriggerWidth;
-
-            _rightBorder = Screen.width - _leftBorder;
-            _topBorder = Screen.height - _bottomBorder;
-        }
-
-        //private void CheckForScreenSizeChange()
-        //{
-        //    Debug.Log("проверяется размер экрана");
-        //        //добавить переменную для хранения размера экрана и сравнить старое ее значение с текущим
-        //}
-
-        //private void InvokeRepeating(Action method, float time, float repeatRate)
-        //{
-        //    InvokeRepeating(method.Method.Name, time, repeatRate);
-        //}
+        
 
     }
 }
---
+++ 180601_TD/Assets/Scripts/CameraMovement/Platform Dependent Input/CameraMovementInputWindowsPC.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Game.CameraMovement
+{
+
+    public class CameraMovementInputWindowsPC : ICameraMovementInput
+    {
+        private Vector3 _mousePosition;
+        private float _horizontalAxisValue;
+        private float _verticalAxisValue;
+        private float _mouseScrollAxisValue;
+
+        private float _rightBorder;
+        private float _leftBorder;
+        private float _topBorder;
+        private float _bottomBorder;
+
+        public CameraMovementInputWindowsPC(float mouseMoveSideTriggerWidth)
+        {
+            _leftBorder = Screen.width * mouseMoveSideTriggerWidth;
+            _bottomBorder = Screen.height * mouseMoveSideTriggerWidth;
+
+            _rightBorder = Screen.width - _leftBorder;
+            _topBorder = Screen.height - _bottomBorder;
+        }
+
+        public void GetInput(ref Vector3 cameraMovementVector, ref float cameraZoomValue)
+        {
+            //движение камеры
+            cameraMovementVector = Vector3.zero;
+
+            _mousePosition = Input.mousePosition;
+
+            _horizontalAxisValue = Input.GetAxisRaw("Horizontal");
+            _verticalAxisValue = Input.GetAxisRaw("Vertical");
+
+            if (_mousePosition.x <= _leftBorder || _horizontalAxisValue < 0)
+                cameraMovementVector += new Vector3(-1, 0, 0);
+
+            if (_mousePosition.x >= _rightBorder || _horizontalAxisValue > 0)
+                cameraMovementVector += new Vector3(1, 0, 0);
+
+            if (_mousePosition.y <= _bottomBorder || _verticalAxisValue < 0)
+                cameraMovementVector += new Vector3(0, 0, -1);
+
+            if (_mousePosition.y >= _topBorder || _verticalAxisValue > 0)
+                cameraMovementVector += new Vector3(0, 0, 1);
+
+            //зум
+            cameraZoomValue = Input.GetAxis("Mouse ScrollWheel");
+        }
+    }
+}
